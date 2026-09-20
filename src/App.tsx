f3b2356b4fbee48a494534645c7e365ca55a74ae
Jono Oliver <jonooliver@Jonos-MacBook-Pro.local>
2026-09-20 13:48:53 +0100

    Final scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       </p>
       <button
         type="button"
-        className="rounded-lg bg-violet-600 px-5 py-2.5 font-medium text-white transition hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-400"
+        className="rounded-lg bg-violet-600 px-5 py-2.5 font-medium text-white transition hover:bg-violet-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-violet-400"
         onClick={() => setCount((c) => c + 1)}
       >
         Count is {count}
